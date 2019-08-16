8cd68f8e04429ce34a0f491e69988dec0efc3371
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2019-08-16 20:09:09 -0300

    feat: add randomRestaurant feature

--- api/controllers/MyCuisinessController.js
+++ api/controllers/MyCuisinessController.js
@@ -5,13 +5,27 @@ class MyCuisinessController {
     this.zomatoRepository = new ZomatoRepository();
   }
 
-  async getCategories() {
-    const response = await this.zomatoClient.categories();
-    const categoriesObject = {};
-    response.categories.forEach((category) => {
-      categoriesObject[category.categories.id] = category.categories.name;
-    });
-    return categoriesObject;
+  async getRandomRestaurant(params) {
+    const categories = await this.zomatoRepository.getCategories();
+    const category = this.randomCategory(categories);
+    
+    Object.assign(params, { category, radius: 2000 })
+    const restaurants = await this.zomatoRepository.search(params);
+    const restaurant = this.randomRestaurant(restaurants);
+
+    return {
+      name: restaurant.name,
+      location: restaurant.location
+    };
+  }
+
+  randomCategory(categories) {
+    const categoriesIds = Object.keys(categories);
+    return categoriesIds[Math.floor(Math.random() * categoriesIds.length)];
+  }
+
+  randomRestaurant(restaurants) {
+    return restaurants[Math.floor(Math.random() * restaurants.length)].restaurant;
   }
 }
 
